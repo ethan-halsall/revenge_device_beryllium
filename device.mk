9b691dec236d080cf562432c6f1585bfca8ee040
ethan-halsall <ethan.halsall@gmail.com>
2019-06-26 09:54:50 +0100

    Bringup Revenge os
    Signed-off-by: ethan-halsall <ethan.halsall@gmail.com>

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-revenge
 
 # Properties
 -include $(LOCAL_PATH)/device-props.mk
